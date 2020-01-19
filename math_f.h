15a19b3e2d0e8499c26a3ec766e0bdfd0bf04ff2
znseday <55926888+znseday@users.noreply.github.com>
2020-01-19 04:36:16 +0300

    Add files via upload

--- math_f.h
+++ math_f.h
@@ -1,7 +1,7 @@
 #ifndef MATH_F_H
 #define MATH_F_H
 
-unsigned long long MyFa(unsigned long long n);
-unsigned long long MyFi(unsigned long long n);
+unsigned long long fa(unsigned long long n);
+unsigned long long fi(unsigned long long n);
 
 #endif // MATH_F_H
